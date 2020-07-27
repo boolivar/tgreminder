b609294bd1cf30ecfd5dd2bd8cffe85e805f8e4f
Aleksey Krichevskiy <boolivar@gmail.com>
2020-07-28 00:24:39 +0300

    UpdateListener

---
+++ src/main/java/org/bool/tgreminder/service/UpdateListener.java
@@ -0,0 +1,33 @@
+package org.bool.tgreminder.service;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+import com.pengrad.telegrambot.TelegramBot;
+import com.pengrad.telegrambot.UpdatesListener;
+import com.pengrad.telegrambot.model.Update;
+
+@Component
+public class UpdateListener implements UpdatesListener {
+
+    private final UpdateService updateService;
+    
+    public UpdateListener(UpdateService updateService) {
+        this.updateService = updateService;
+    }
+    
+    @Autowired
+    public void register(TelegramBot telegramBot) {
+        telegramBot.setUpdatesListener(this);
+    }
+    
+    @Override
+    public int process(List<Update> updates) {
+        for (Update update : updates) {
+            updateService.update(null, update);
+        }
+        return CONFIRMED_UPDATES_ALL;
+    }
+}
