50bae5ef103979cd104cb758027b493c2e479953
Aleksey Krichevskiy <boolivar@gmail.com>
2020-09-16 23:55:49 +0300

    MessageParser

---
+++ src/main/java/org/bool/tgreminder/core/MessageParser.java
@@ -0,0 +1,37 @@
+package org.bool.tgreminder.core;
+
+import org.apache.commons.lang3.StringUtils;
+import org.bool.tgreminder.dto.ReminderDto;
+import org.springframework.stereotype.Component;
+
+import java.time.OffsetDateTime;
+
+@Component
+public class MessageParser {
+
+    public ReminderDto parse(String text) {
+        String[] parts = StringUtils.splitByWholeSeparator(text, " ", 3);
+        
+        if ("/start".equals(parts[0])) {
+            return instantMessage("Hello!");
+        }
+        
+        if ("/remind".equals(parts[0])) {
+            if (parts.length != 3) {
+                return instantMessage("Invalid request");
+            }
+            
+            try {
+                return new ReminderDto(OffsetDateTime.parse(parts[1]), parts[2]);
+            } catch (RuntimeException e) {
+                return instantMessage("Invalid request param");
+            }
+        }
+        
+        return instantMessage("Unknown command: " + parts[0]);
+    }
+    
+    private ReminderDto instantMessage(String text) {
+        return new ReminderDto(OffsetDateTime.now(), text);
+    }
+}
--- src/main/java/org/bool/tgreminder/service/UpdateService.java
+++ src/main/java/org/bool/tgreminder/service/UpdateService.java
@@ -2,38 +2,32 @@ package org.bool.tgreminder.service;
 
 import com.pengrad.telegrambot.model.Update;
 
-import org.apache.commons.lang3.StringUtils;
+import org.bool.tgreminder.core.MessageParser;
 import org.bool.tgreminder.core.Reminder;
+import org.bool.tgreminder.dto.ReminderDto;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
-
-import java.time.OffsetDateTime;
 
 @Service
 public class UpdateService {
     
     private static final Logger log = LoggerFactory.getLogger(UpdateService.class);
 
+    private final MessageParser messageParser;
+    
     private final Reminder reminder;
     
-    public UpdateService(Reminder reminder) {
+    public UpdateService(MessageParser messageParser, Reminder reminder) {
+        this.messageParser = messageParser;
         this.reminder = reminder;
     }
     
     public void update(String key, Update update) {
         log.info("Update {} {}", key, update);
         
-        String text = readMessage(update.message().text());
-        if (StringUtils.isNotBlank(text)) {
-            reminder.remind(update.message().chat().id(), text, OffsetDateTime.now().plusSeconds(10));
-        }
-    }
-    
-    private String readMessage(String text) {
-        if (StringUtils.equals(text, "/start")) {
-            return "Hello!";
-        }
-        return StringUtils.substringAfter(text, "/start ");
+        ReminderDto message = messageParser.parse(update.message().text());
+        
+        reminder.remind(update.message().chat().id(), message.getMessage(), message.getTime());
     }
 }
