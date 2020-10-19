88ffca91c8176cc66dd51ae44e5b35cb26f7269e
Aleksey Krichevskiy <boolivar@gmail.com>
2020-10-19 03:06:18 +0300

    WebConfig: use gson for http message conversion

---
+++ src/main/java/org/bool/tgreminder/config/WebConfig.java
@@ -0,0 +1,17 @@
+package org.bool.tgreminder.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.converter.HttpMessageConverter;
+import org.springframework.http.converter.json.GsonHttpMessageConverter;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+import java.util.List;
+
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+
+    @Override
+    public void extendMessageConverters(List<HttpMessageConverter<?>> converters) {
+        converters.add(0, new GsonHttpMessageConverter());
+    }
+}
