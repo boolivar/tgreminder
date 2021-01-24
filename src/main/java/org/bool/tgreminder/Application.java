7338ac61497d694428f275ec43235359136affc7
Aleksey Krichevskiy <boolivar@gmail.com>
2021-01-24 16:57:52 +0300

    application.zone-id property

--- src/main/java/org/bool/tgreminder/Application.java
+++ src/main/java/org/bool/tgreminder/Application.java
@@ -1,24 +1,29 @@
 package org.bool.tgreminder;
 
 import org.springframework.aop.framework.autoproxy.DefaultAdvisorAutoProxyCreator;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
 import java.time.Clock;
+import java.time.ZoneId;
 
 @EnableScheduling
 @SpringBootApplication
 public class Application {
 
+    @Value("${application.zone-id:Europe/Moscow}")
+    private String zoneId;
+    
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
     }
     
     @Bean
     public Clock clock() {
-        return Clock.systemUTC();
+        return Clock.system(ZoneId.of(zoneId));
     }
     
     @Bean
