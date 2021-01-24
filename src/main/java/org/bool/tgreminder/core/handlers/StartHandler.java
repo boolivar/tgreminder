2023d53bb6184957c0eccf0a67545dfd240df14e
Aleksey Krichevskiy <boolivar@gmail.com>
2021-01-24 15:08:55 +0300

    StartHandler: insert user

--- src/main/java/org/bool/tgreminder/core/handlers/StartHandler.java
+++ src/main/java/org/bool/tgreminder/core/handlers/StartHandler.java
@@ -2,7 +2,9 @@ package org.bool.tgreminder.core.handlers;
 
 import org.bool.tgreminder.core.CommandHandler;
 import org.bool.tgreminder.core.ReminderFactory;
+import org.bool.tgreminder.core.UserRepository;
 import org.bool.tgreminder.dto.ReminderDto;
+import org.bool.tgreminder.dto.UserDto;
 import org.bool.tgreminder.i18n.Messages;
 import org.springframework.stereotype.Component;
 
@@ -11,13 +13,20 @@ public class StartHandler implements CommandHandler {
 
     private final ReminderFactory reminderFactory;
     
-    public StartHandler(ReminderFactory reminderFactory) {
+    private final UserRepository userRepository;
+    
+    public StartHandler(ReminderFactory reminderFactory, UserRepository userRepository) {
         this.reminderFactory = reminderFactory;
+        this.userRepository = userRepository;
     }
     
     @Override
     public ReminderDto handle(Integer userId, Long chatId, String[] args) {
         if ("/start".equals(args[0])) {
+            UserDto user = userRepository.findById(userId);
+            if (user == null) {
+                userRepository.insertUser(new UserDto(userId, null, null));
+            }
             return reminderFactory.instantMessage(Messages.HELLO);
         }
         return null;
