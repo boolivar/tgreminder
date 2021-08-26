2f551cab63b8c00f9dc9e5566e938cc057792d33
Aleksey Krichevskiy <boolivar@gmail.com>
2021-08-26 04:15:29 +0300

    DateTimeParser updated

--- src/main/java/org/bool/tgreminder/core/DateTimeParser.java
+++ src/main/java/org/bool/tgreminder/core/DateTimeParser.java
@@ -1,58 +1,38 @@
 package org.bool.tgreminder.core;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Component;
-
 import java.time.Clock;
-import java.time.LocalDateTime;
+import java.time.LocalDate;
 import java.time.LocalTime;
 import java.time.OffsetDateTime;
-import java.time.OffsetTime;
 import java.time.ZoneId;
-import java.time.format.DateTimeFormatter;
-import java.time.format.DateTimeFormatterBuilder;
-import java.time.temporal.TemporalAccessor;
+import java.time.ZonedDateTime;
+
+import org.springframework.stereotype.Component;
 
 @Component
 public class DateTimeParser {
 
-    private final DateTimeFormatter formatter;
+    private final TimeParser timeParser;
     
     private final Clock clock;
     
-    @Autowired
-    public DateTimeParser(Clock clock) {
-        this(new DateTimeFormatterBuilder()
-            .optionalStart()
-                .append(DateTimeFormatter.ISO_LOCAL_DATE).appendLiteral('T')
-            .optionalEnd()
-            .append(DateTimeFormatter.ISO_LOCAL_TIME)
-            .optionalStart()
-                .appendOffset("+HH:mm", "Z")
-            .optionalEnd()
-            .toFormatter(), clock);
-    }
-
-    public DateTimeParser(DateTimeFormatter formatter, Clock clock) {
-        this.formatter = formatter;
+    public DateTimeParser(TimeParser timeParser, Clock clock) {
+        this.timeParser = timeParser;
         this.clock = clock;
     }
     
-    public OffsetDateTime parse(String text, String userTimeZone) {
-        return parse(text, userTimeZone != null ? ZoneId.of(userTimeZone) : clock.getZone());
+    public OffsetDateTime parseInstant(String instant, ZoneId zoneId) {
+        return timeParser.parseDateTime(instant);
     }
     
-    public OffsetDateTime parse(String text, ZoneId userZoneId) {
-        TemporalAccessor value = formatter.parseBest(text, OffsetDateTime::from, LocalDateTime::from, OffsetTime::from, LocalTime::from);
-        if (value instanceof OffsetDateTime) {
-            return (OffsetDateTime) value;
-        }
-        if (value instanceof LocalDateTime) {
-            return ((LocalDateTime) value).atZone(userZoneId).toOffsetDateTime();
-        }
-        if (value instanceof OffsetTime) {
-            return ((OffsetTime) value).atDate(clock.instant().atZone(userZoneId).toLocalDate());
-        }
-        return clock.instant().atZone(userZoneId).with((LocalTime) value).toOffsetDateTime();
+    public OffsetDateTime parseTime(String time, ZoneId zoneId) {
+        LocalTime localTime = timeParser.parseTime(time);
+        return ZonedDateTime.now(clock).withZoneSameInstant(zoneId).with(localTime).toOffsetDateTime();
+    }
+    
+    public OffsetDateTime parseDateTime(String date, String time, ZoneId zoneId) {
+        LocalDate localDate = timeParser.parseDate(date);
+        LocalTime localTime = timeParser.parseTime(time);
+        return ZonedDateTime.of(localDate, localTime, zoneId).toOffsetDateTime();
     }
 }
--- src/test/java/org/bool/tgreminder/core/DateTimeParserTest.java
+++ src/test/java/org/bool/tgreminder/core/DateTimeParserTest.java
@@ -1,49 +1,65 @@
 package org.bool.tgreminder.core;
 
-import org.junit.jupiter.api.TestInstance;
-import org.junit.jupiter.api.TestInstance.Lifecycle;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+
+import java.time.Clock;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.LocalTime;
+import java.time.OffsetDateTime;
+import java.time.ZoneOffset;
+
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
 
-import java.time.Clock;
-import java.time.LocalDateTime;
-import java.time.OffsetDateTime;
-import java.time.ZoneId;
-import java.time.ZoneOffset;
-
-import static org.junit.jupiter.api.Assertions.assertEquals;
-
-@TestInstance(Lifecycle.PER_CLASS)
 class DateTimeParserTest {
 
-    private final OffsetDateTime time = OffsetDateTime.of(LocalDateTime.of(2001, 5, 1, 22, 30), ZoneOffset.UTC);
+    private final Clock clock = Clock.fixed(LocalDateTime.parse("2020-05-01T22:21").toInstant(ZoneOffset.UTC), ZoneOffset.UTC);
     
-    private final Clock clock = Clock.fixed(time.toInstant(), ZoneOffset.UTC);
-    
-    private final DateTimeParser parser = new DateTimeParser(clock);
+    private final TimeParser timeParser = mock(TimeParser.class);
+
+    private final DateTimeParser parser = new DateTimeParser(timeParser, clock);
     
     @CsvSource({
-        "-2, 2001-05-01T23:10:00-02:00",
-        "-1, 2001-05-01T23:10:00-01:00",
-        "+1, 2001-05-01T23:10:00+01:00",
-        
-        "+2, 2001-05-02T23:10:00+02:00",
-        "+3, 2001-05-02T23:10:00+03:00"
+        "2020-05-01T10:30-01:00,    10:30,  -1",
+        "2020-05-01T20:15:00Z,      20:15,   0",
+        "2020-05-02T23:10:00+05:00, 23:10,  +5",
     })
     @ParameterizedTest
-    void testUserZone(String timeZone, String expected) {
-        assertEquals(OffsetDateTime.parse(expected), parser.parse("23:10", ZoneId.of(timeZone)));
+    void testParseTime(OffsetDateTime expected, LocalTime time, int offset) {
+        when(timeParser.parseTime("<time>"))
+                .thenReturn(time);
+        
+        assertEquals(expected, parser.parseTime("<time>", ZoneOffset.ofHours(offset)));
     }
     
     @CsvSource({
-        "2007-12-03T10:15:30+01:00, 2007-12-03T10:15:30+01:00",
-        "2015-10-10T08:00,          2015-10-10T08:00:00+03:00",
-        "10:20+01:00,               2001-05-02T10:20:00+01:00",
-        "10:20+02:00,               2001-05-02T10:20:00+02:00",
-        "23:15:20,                  2001-05-02T23:15:20+03:00"
+        "2020-05-01T23:10:00+03:00, -1",
+        "2020-05-01T23:10:00+03:00,  0",
+        "2020-05-01T23:10:00+03:00, +5",
     })
     @ParameterizedTest
-    void testParseValues(String text, String expected) {
-        assertEquals(OffsetDateTime.parse(expected), parser.parse(text, ZoneOffset.ofHours(3)));
+    void testParseInstant(OffsetDateTime expected, int offset) {
+        when(timeParser.parseDateTime("<instant>"))
+                .thenReturn(expected);
+        
+        assertEquals(expected, parser.parseInstant("<instant>", ZoneOffset.ofHours(offset)));
+    }
+    
+    @CsvSource({
+        "2020-05-01T10:30-01:00,    2020-05-01, 10:30,  -1",
+        "2021-02-01T20:15:00Z,      2021-02-01, 20:15,   0",
+        "2000-05-05T23:10:00+05:00, 2000-05-05, 23:10,  +5",
+    })
+    @ParameterizedTest
+    void testParseDateTime(OffsetDateTime expected, LocalDate date, LocalTime time, int offset) {
+        when(timeParser.parseDate("<date>"))
+                .thenReturn(date);
+        when(timeParser.parseTime("<time>"))
+                .thenReturn(time);
+        
+        assertEquals(expected, parser.parseDateTime("<date>", "<time>", ZoneOffset.ofHours(offset)));
     }
 }
