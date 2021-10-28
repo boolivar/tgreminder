51c9cb37d3d68013ace31a216337ff7f572563f0
Aleksey Krichevskiy <boolivar@gmail.com>
2021-10-29 02:01:29 +0300

    chats db table

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -1,6 +1,8 @@
-create table USERS(ID bigint, NAME varchar(256), TIME_ZONE varchar(128));
+create table USERS(ID bigint PRIMARY KEY, NAME varchar(256), TIME_ZONE varchar(128));
 
-create table REMINDERS(ID bigint, CHAT_INDEX integer, USER_ID bigint, CHAT_ID bigint, MESSAGE varchar(256), TIME timestamp, FLAGS varchar(128));
+create table CHATS(ID bigint PRIMARY KEY, CHAT_INDEX integer);
+
+create table REMINDERS(ID bigint PRIMARY KEY, CHAT_INDEX integer, USER_ID bigint, CHAT_ID bigint, MESSAGE varchar(256), TIME timestamp, FLAGS varchar(128), UNIQUE (CHAT_ID, CHAT_INDEX));
 
 create sequence USERS_SEQ;
 
